6cd29ca25de5133916bed20c660efd9d7c9caa2f
Tobias Rodriguez <63003853+totobias2002@users.noreply.github.com>
2025-10-08 00:54:32 -0300

    cambio java

---
+++ algorithms-II/src/main/java/org/uade/algorithm/Main.java
@@ -0,0 +1,8 @@
+package org.uade.algorithm;
+
+public class Main {
+    public static void main(String[] args) {
+        Menu menu = new Menu();
+        menu.iniciar();
+    }
+}
---
+++ algorithms-II/src/main/java/org/uade/algorithm/Menu.java
@@ -0,0 +1,12 @@
+package org.uade.algorithm;
+
+public class Menu {
+    public void iniciar() {
+        System.out.println("==== SISTEMA DE RESTAURANTE ====");
+        System.out.println("1. Registrar pedido");
+        System.out.println("2. Preparar platos");
+        System.out.println("3. Asignar pedido a repartidor");
+        System.out.println("4. Generar reportes");
+        System.out.println("0. Salir");
+    }
+}
